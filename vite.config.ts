77ce35021f5982aca5f971ac34605a3b33d50e6c
Felipe Urrutia <thewomins@gmail.com>
2022-11-10 18:14:04 -0300

    Add build option for multiple pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
-export default {
+import {resolve} from "path";
+import {defineConfig} from "vite";
+
+export default defineConfig({
   // config options
   build: {
     target: "esnext",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        carrito: resolve(__dirname, "pages/carrito.html"),
+      },
+    },
   },
-};
+});
